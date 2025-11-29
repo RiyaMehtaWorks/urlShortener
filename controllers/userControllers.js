d62b033b95a031257d0aa8879691330958387cd3
Riya Mehta <riyamehtaatwork@gmail.com>
2025-11-29 07:17:26 +0530

    basic login setup without auth implementation

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,9 +3,21 @@ const User = require("../models/userModel");
 async function handleUserSignup(req, res) {
   const { name, email, password } = req.body;
   await User.create({ name, email, password });
-  return res.render("home");
+  return res.redirect("/");
+}
+
+async function handleUserLogin(req, res) {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email, password });
+  if (!user) {
+    return res.render("login", {
+      error: "Invalid email or password",
+    });
+  }
+  return res.redirect("/");
 }
 
 module.exports = {
   handleUserSignup,
+  handleUserLogin,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { handleUserSignup } = require("../controllers/userControllers");
+const {
+  handleUserSignup,
+  handleUserLogin,
+} = require("../controllers/userControllers");
 router.post("/", handleUserSignup);
+router.post("/login", handleUserLogin);
 
 module.exports = router;
